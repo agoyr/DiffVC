c7bcfe06960f2fded71ba23e4996cbeb7d463521
agoyr <yamauchi.ryoga116@gmail.com>
2023-12-05 15:01:43 +0900

    fix train_dec.py

--- DiffVC/train_dec.py
+++ DiffVC/train_dec.py
@@ -46,7 +46,7 @@ data_dir = './data/'
 val_file = 'filelists/validvc.txt'
 exc_file = 'filelists/exceptions_vctk.txt'
 
-log_dir = 'logs_dec_VCTK1'
+log_dir = 'logs_dec_VCTK2'
 enc_dir = 'logs_enc'
 epochs = 110
 accum_iter = 4
